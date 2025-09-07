f9e3b8d26e22b6b795b35c203f1117afde90ba72
Przemysław Bukala <bukalaprzemyslaw@gmail.com>
2025-09-07 12:42:26 +0200

    Remove public directory from repo

--- src/main.py
+++ src/main.py
@@ -5,21 +5,11 @@ import textnode
 from textnode import TextType
 from textnode import TextNode
 import logging
-
-
-
-
-
-
-
 def copy_static():
     """
     Recursive function that copies all the contents from a source directory to a destination directory (static to public)
     It deletes all the contents of the destination directory (public) to ensure that the copy is clean and copy all files and subdirectories, nested files, itd.
     It logs the path of each file you copy in
-
-    :return:
-
     """
     logging.basicConfig(filename="logger.log",format='%(asctime)s %(message)s',filemode='w')
     logger = logging.getLogger()
@@ -33,18 +23,11 @@ def copy_static():
     if not os.path.exists('static/'):
         logger.error("Path: static  do not exist")
         raise Exception('Path: static/  do not exist')
-
-
-     ## os.listdir to mozna uzyc do loggera  tak samo to: os.path.join
-
-    dir_list=os.listdir()
     dir_list=os.listdir('static/')
-    print(dir_list)
-    # paths=os.path.join(dir_list)
-    # print(paths)
+    recursive_coping('static','public',logger)
 
 
-def recursive_coping(src_directory,destination):
+def recursive_coping(src_directory: str,destination, logger):
     """
     Recursive function that copies all the contents from a source directory to a destination directory.
     :param src_directory:
@@ -52,15 +35,19 @@ def recursive_coping(src_directory,destination):
     :param destination:
     :type: str
     """
-    ## os.listdir to mozna uzyc do loggera  tak samo to: os.path.join
-    # TU sie przyda JOIN !!!!!!!!
-    # TODO dokonczyc to
     dir_list=os.listdir(src_directory)
+
     for i in dir_list:
-        if i
-
-    pass
-
+        full_path=os.path.join(src_directory,i)
+        int(os.path.isdir(full_path))
+        if os.path.isfile(full_path):
+            logger.info(f"Coping file {full_path}")
+            shutil.copy(full_path,destination)
+        elif os.path.isdir(full_path):
+            directory_to_destination = os.path.join(destination, i)
+            os.mkdir(directory_to_destination)
+            logger.info(f"Making directory {directory_to_destination}")
+            recursive_coping(full_path,directory_to_destination,logger)
 
 if __name__=="__main__":
     copy_static()
