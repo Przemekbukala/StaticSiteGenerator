2b9fd871bc2487ea2942e20d68bd2e472cc6e348
Przemysław Bukala <bukalaprzemyslaw@gmail.com>
2025-11-29 21:26:38 +0100

    Change docs contents

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DIR=${1:-'/StaticSiteGenerator/'}
 python3 src/main.py "$DIR"
-cd docs && python3 -m http.server 8888
 if [ "$BASEPATH" = "/" ]; then
     echo "Running local server at http://0.0.0.0:8888"
     cd docs && python3 -m http.server 8888
